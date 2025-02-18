0f90903bd3f303460d9798e381fc6e9b1120da61
Supun Pramod <pramodsupun06@gmail.com>
2025-02-18 23:34:57 +0530

    create the toggle navbar for js

---
+++ main.js
@@ -0,0 +1,9 @@
+// toggle navbar
+let menuIcon=document.querySelector('#menu-icon');
+let navbar=document.querySelector('.navbar');
+
+menuIcon.onclick=()=>{
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
+
